a9d52c68a8a0e2072ca6232811a9caf873c299c0
Matt McCarrick <matt.mccarrick@openventana.com>
2016-02-14 15:53:15 -0600

    Complete advent 2
    Finished up advent 2

--- advent2.rb
+++ advent2.rb
@@ -1,26 +1,22 @@
 input = File.readlines("input/advent2.txt")
 
 class Dimension_parser
-  @input
   def initialize(input)
     @input = input
   end
   def parse
-    @dimensions = []
+    @sides_to_use = []
     @input.each do |line|
       split_line = line.split("x")
       #puts line
-      @dimensions << Dimension_model.new(split_line[0], split_line[1], split_line[2])
+      @sides_to_use << Dimension_model.new(split_line[0], split_line[1], split_line[2])
     end
-    puts @dimensions[1].to_s
-    return @dimensions
+    puts @sides_to_use[1].to_s
+    return @sides_to_use
   end
 end
 
 class Dimension_model
-  @length
-  @width
-  @height
   def initialize(length, width, height)
     @length = length.to_i
     @width = width.to_i
@@ -44,7 +40,15 @@ class Dimension_model
   end
 
   def get_ribbon_length
-    [side1, side2, side3]-[side1, side2, side3].max
+    sides_to_use = [@length, @width, @height]
+    length = 0
+    sides_to_use.slice!(sides_to_use.index(sides_to_use.max))
+    puts(sides_to_use.to_s)
+    sides_to_use.each{|side|
+      length += 2*side
+    }
+    length += @length * @width * @height
+    return length
   end
 
   def to_s
@@ -55,7 +59,12 @@ end
 parser = Dimension_parser.new(input)
 boxes = parser.parse
 total_surface = 0
+total_ribbon_length = 0
 boxes.each do |box|
   total_surface += box.getSurfaceArea
 end
 puts total_surface
+
+boxes.each {|box| total_ribbon_length += box.get_ribbon_length}
+
+puts total_ribbon_length
